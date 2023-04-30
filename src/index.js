7f89ac81cb7ca0bc7c966d2799ebc2071a87fa5a
“Xhesika” <xhesikam2184@tywls.org>
2023-04-29 22:17:17 -0400

    got login working

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 const server = require('./server');
 
-const port = process.env.PORT || 3000;
-const host = process.env.HOST || '127.0.0.1';
+const port = process.env.PG_PORT || 3000;
+const host = process.env.PG_HOST || '127.0.0.1';
 
 server.listen(port, host, () => {
   console.log(`Server running at http://${host}:${port}/`);
