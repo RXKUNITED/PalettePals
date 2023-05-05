69a116b411b86ad865b326a89c731ce5d57acfeb
OrwRandal <randypichardo1987@gmail.com>
2023-05-05 15:59:14 -0400

    made posts pop up on main

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -5,6 +5,8 @@ import {
   setNav,
 } from "./global.js";
 
+const postDiv = document.querySelector('#postDiv');
+
 const redirectToLogin = () => window.location.assign("/login.html"); 
 
 const main = async () => {
@@ -14,13 +16,71 @@ const main = async () => {
 }
 
 const getPosts = async () => {
-  // const user = await fetchLoggedInUser()
-  // console.log(user.id)
-  
-  // console.log(posts)
-  let data =  await handleFetch('/posts');
-  console.log(data)
+  let data =  await handleFetch('api/posts');
+  return data[0];
 }
+
+const getLikes = async (id) => {
+  let likeAmount = await handleFetch(`api/posts/${id}/likes/`);
+  return likeAmount[0]["like_count"];
+}
+
+addEventListener("DOMContentLoaded", async (event) => {
+  let posts = await getPosts();
+  for(let post of posts){
+    let likes = await getLikes(post.id);
+
+    let cardDiv = document.createElement('div');
+    let imageDiv = document.createElement('div');
+    let cardFigure = document.createElement('figure');
+    let image = document.createElement('img')
+    let cardContent = document.createElement('div');
+    let content = document.createElement('div');
+    let pContent = document.createElement('p');
+    let footer = document.createElement('footer');
+    let footerLike = document.createElement('a');
+    let footerEdit = document.createElement('a');
+    let footerDelete = document.createElement('a');
+    let likeCount = document.createElement('p')
+    likeCount.innerText = `Likes: ` + likes;;
+
+    cardDiv.classList.add("card");
+    imageDiv.classList.add("card-image");
+    cardFigure.classList.add("image");
+    cardContent.classList.add("card-content");
+    content.classList.add("content");
+    footer.classList.add("card-footer");
+    image.src = post.img_url;
+    pContent.innerText = `${post.username}: ${post.caption}`;
+    footerLike.classList.add('card-footer-item');
+    footerLike.innerText = 'Like';
+    footerLike.href = '#';
+    footerEdit.classList.add('card-footer-item');
+    footerEdit.innerText = 'Edit';
+    footerEdit.href = '#';
+    footerDelete.classList.add('card-footer-item');
+    footerDelete.innerText = 'Delete';
+    footerDelete.href = '#';
+
+    cardDiv.classList.add('my-4');
+
+    cardDiv.append(imageDiv);
+    imageDiv.append(cardFigure);
+    cardFigure.append(image);
+    cardDiv.append(cardContent);
+    cardContent.append(content);
+    content.append(pContent);
+    content.append(likeCount);
+    cardDiv.append(footer);
+    footer.append(footerLike);
+    footer.append(footerEdit);
+    footer.append(footerDelete);
+    postDiv.append(cardDiv);
+  }
+});
+
+
+
 
 main()
 getPosts();
--- src/db/seeds/02_posts.js
+++ src/db/seeds/02_posts.js
@@ -13,7 +13,7 @@ exports.seed = async function(knex) {
   
   await knex('posts').insert([
     {id: 1, user_id: 1, img_url: "https://cdn.shopify.com/s/files/1/1551/3581/files/Artist_Thumbnail_-__0004_PICASSO_b1073_jacqueline_in_a_straw_hat_lipic1073sc_un1.jpg?v=1659989054`",caption: "hi"},
-    {id: 2, user_id: 2, img_url: "", caption: "test"},
-    {id: 3, user_id: 2, img_url: "", caption: "test"},
+    {id: 2, user_id: 2, img_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg/1200px-Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg", caption: "test"},
+    {id: 3, user_id: 2, img_url: "https://cdn.britannica.com/78/43678-050-F4DC8D93/Starry-Night-canvas-Vincent-van-Gogh-New-1889.jpg", caption: "test"},
   ]);
 };
